38b26f0b9b8e5501778641488838603c97621217
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-01-06 07:48:23 +0200

    Setup for Travis

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var sass = require('dart-sass');
 
-scss_filename = "basic.sass"
+scss_filename = "basic.sass";
 
-sass.render({data: 'body{background:blue; a{color:black;}}', function(err, result) {
-  console.log(result);
-  console.log(err);
-});
+console.log(sass);
