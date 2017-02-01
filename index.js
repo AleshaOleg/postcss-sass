7e4c281bc83fc8b97449db580cb6ac763b740fc7
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-02-01 23:09:33 +0200

    Import dart-sass

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ scss_filename = "./basic.sass";
 // Example of using dart-sass
 const result = sass.renderSync({file: scss_filename});
 console.log((result.buffer).toString());
+console.log(sass.render);
 
 module.exports = sass;
