31bbb6bf87aa39575b4e1fde8abc980c89abc6f6
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-02-02 00:32:31 +0200

    Reverse changes

--- index.dart
+++ index.dart
@@ -1,21 +1,5 @@
-import 'package:path/path.dart' as p;
-
-import 'package:sass/src/ast/sass.dart';
-import 'package:sass/src/exception.dart';
-import 'package:sass/src/utils.dart';
-import 'package:sass/src/visitor/perform.dart';
-import 'package:sass/src/visitor/serialize.dart';
-
-String sassAst(String path, {bool color: false}) {
-    var contents = readSassFile(path);
-    var url = p.toUri(path);
-    var sassTree = p.extension(path) == '.sass'
-        ? new Stylesheet.parseSass(contents, url: url, color: color)
-        : new Stylesheet.parseScss(contents, url: url, color: color);
-    var cssTree = evaluate(sassTree, color: color);
-    return toCss(cssTree);
-  }
+import 'package:sass/sass.dart' as sass;
 
 void main(List<String> args) {
-  print(sassAst(args.first));
+  print(sass.render(args.first));
 }
--- index.js
+++ index.js
@@ -6,6 +6,5 @@ scss_filename = "./basic.sass";
 // Example of using dart-sass
 const result = sass.renderSync({file: scss_filename});
 console.log((result.buffer).toString());
-console.log(sass.render);
 
 module.exports = sass;
