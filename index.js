15fd3b1bca9ceb33ec8a214b8937bf4d50a371b1
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-02-13 10:03:44 +0200

    Get all nodes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var gonzales = require('gonzales-pe');
+var postcss = require('postcss');
 var fs = require('fs');
+
+let nodes = [];
+
+function getNodes(node) {
+  if (node.hasOwnProperty('content') && typeof node.content === 'object') {
+    node.content.map((props) => {
+      if (/^[a-zA-Z]+$/.test(props.content) && typeof props.content !== 'object') {
+        nodes.push(props);
+      } else {
+        getNodes(props);
+      }
+    })
+  }
+}
 
 process.argv.forEach(function (val, index, array) {
   if (index > 1) {
@@ -10,6 +25,15 @@ process.argv.forEach(function (val, index, array) {
       // Parse Sass file, and get Sass AST
       var sassAST = (gonzales.parse(data, {syntax: 'sass'}));
       console.log(sassAST);
+      getNodes(sassAST);
+      console.log('-----');
+      console.log(nodes);
+      console.log('-----');
     });
   }
 });
+
+const root = postcss.parse('a { background: white } a { background: black }');
+console.log('-----');
+console.log(root.nodes);
+console.log('-----');
