8a88a6df2b2851a9ec3bfea5300b3d804951604f
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-03-20 02:01:24 +0200

    Added files to test with. Fixed bugs with selectors and included rules

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,11 @@
 var postcss = require('postcss');
 var gonzales = require('gonzales-pe');
 
-it('PostCSS dependency', () => {
+it('PostCSS dependency', function () {
     expect(typeof postcss).toBe('function');
 });
 
-it('gonzales-pe dependency', () => {
+it('gonzales-pe dependency', function () {
     expect(typeof gonzales).toBe('object');
 });
+
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var fs = require('fs');
+
 var postcss = require('postcss');
 var gonzales = require('gonzales-pe');
 var Input = require('postcss/lib/input');
 
-var sassSource = 'div\n  a\n    span\n      color: red\n  li\n    color: green';
+var sassSource = fs.readFileSync('./__tests__/sass/basic.sass', 'utf-8');
 var sassTree = gonzales.parse(sassSource, { syntax: 'sass' });
-var postCssTree = postcss
-    .parse('div a span { color : red; } div li { color : green; }');
+var cssSource = fs.readFileSync('./__tests__/css/basic.css', 'utf-8');
+var postCssASTFromCss = postcss.parse(cssSource);
 
 var DEFAULT_RAWS_ROOT = {
     semicolon: false,
@@ -25,7 +27,7 @@ var DEFAULT_RAWS_DECL = {
 };
 
 /* eslint-disable complexity */
-function process(node, parent, source, selector) {
+function sassToPostCss(node, parent, source, selector) {
     if (node.type === 'stylesheet') {
         // Create and set parameters for Root node
         var root = postcss.root();
@@ -35,11 +37,11 @@ function process(node, parent, source, selector) {
             source: new Input(source)
         };
         root.raws = DEFAULT_RAWS_ROOT;
-        process(node.content[0], root, source, selector);
-        // Return PostCSS AST
+        for (var i = 0; i < node.content.length; i++) {
+            sassToPostCss(node.content[i], root, source, selector);
+        }
         return root;
     } else if (node.type === 'ruleset') {
-        var last = false;
         // Loop to find the deepest ruleset node
         for (var rContent = 0; rContent < node.content.length; rContent++ ) {
             if (node.content[rContent].type === 'block') {
@@ -50,9 +52,8 @@ function process(node, parent, source, selector) {
                 ) {
                     if (node.content[rContent]
                             .content[bNextContent].type === 'ruleset') {
-                        last = true;
                         // Add to selector value of current node
-                        process(node.content[rContent]
+                        sassToPostCss(node.content[rContent]
                             .content[bNextContent], parent, source, selector);
                     }
                 }
@@ -61,33 +62,51 @@ function process(node, parent, source, selector) {
                 /* Get current selector name
                 It's always have same path,
                 so it's easier to get it without recursion */
-                selector += node.content[rContent]
-                    .content[0].content[0].content + ' ';
+                for (
+                    var sCurrentContent = 0;
+                    sCurrentContent < node.content[rContent].length;
+                    sCurrentContent++
+                ) {
+                    if (node.content[rContent]
+                            .content[sCurrentContent].type === 'id') {
+                        selector += '#' + node.content[rContent]
+                                .content[sCurrentContent].content;
+                    } else if (node.content[rContent]
+                            .content[sCurrentContent].type === 'class') {
+                        selector += '.' + node.content[rContent]
+                                .content[sCurrentContent].content;
+                    } else if (node.content[rContent]
+                            .content[sCurrentContent].type === 'typeSelector') {
+                        selector += node.content[rContent]
+                            .content[sCurrentContent].content;
+                    }
+                }
+                selector += ' ';
             }
         }
 
-        if (!last) {
-            // Create Rule node
-            var rule = postcss.rule();
-            // Object to store raws for Rule
-            var rRaws = {};
+        // Create Rule node
+        var rule = postcss.rule();
+        // Object to store raws for Rule
+        var rRaws = {};
 
-            // Looking for all block nodes in current ruleset node
-            for (
-                var rCurrentContent = 0;
-                rCurrentContent < node.content.length;
-                rCurrentContent++
-            ) {
-                if (node.content[rCurrentContent].type === 'block') {
-                    process(node.content[rCurrentContent], rule, source);
-                } else if (node.content[rCurrentContent].type === 'space') {
-                    if (!rRaws.between) {
-                        rRaws.between = node.content[rCurrentContent].content;
-                    } else {
-                        rRaws.between += node.content[rCurrentContent].content;
-                    }
+        // Looking for all block nodes in current ruleset node
+        for (
+            var rCurrentContent = 0;
+            rCurrentContent < node.content.length;
+            rCurrentContent++
+        ) {
+            if (node.content[rCurrentContent].type === 'block') {
+                sassToPostCss(node.content[rCurrentContent], rule, source);
+            } else if (node.content[rCurrentContent].type === 'space') {
+                if (!rRaws.between) {
+                    rRaws.between = node.content[rCurrentContent].content;
+                } else {
+                    rRaws.between += node.content[rCurrentContent].content;
                 }
             }
+        }
+        if (rule.nodes.length !== 0) {
             // Write selector to Rule, and remove last whitespace
             rule.selector = selector.slice(0, -1);
             // Set parameters for Rule node
@@ -108,7 +127,7 @@ function process(node, parent, source, selector) {
         // Looking for declaration node in block node
         for (var bContent = 0; bContent < node.content.length; bContent++) {
             if (node.content[bContent].type === 'declaration') {
-                process(node.content[bContent], parent, source);
+                sassToPostCss(node.content[bContent], parent, source);
             }
         }
     } else if (node.type === 'declaration') {
@@ -119,7 +138,7 @@ function process(node, parent, source, selector) {
         // Looking for property and value node in declaration node
         for (var dContent = 0; dContent < node.content.length; dContent++) {
             if (node.content[dContent].type === 'property') {
-                process(node.content[dContent], decl);
+                sassToPostCss(node.content[dContent], decl);
             } else if (node.content[dContent].type === 'propertyDelimiter') {
                 if (!dRaws.between) {
                     dRaws.between = node.content[dContent].content;
@@ -133,7 +152,7 @@ function process(node, parent, source, selector) {
                     dRaws.between += node.content[dContent].content;
                 }
             } if (node.content[dContent].type === 'value') {
-                process(node.content[dContent], decl);
+                sassToPostCss(node.content[dContent], decl);
             }
         }
         // Set parameters for Declaration node
@@ -164,7 +183,9 @@ function process(node, parent, source, selector) {
 // Selector for current node
 var selector = '';
 
-var postCssAST = process(sassTree, null, sassSource, selector);
-console.log(postCssAST);
-// console.log('-----');
-console.log(postCssTree);
+var postCssASTFromSass = sassToPostCss(sassTree, null, sassSource, selector);
+console.dir(postCssASTFromCss);
+console.log('-----');
+console.dir(postCssASTFromSass);
+
+module.exports = sassToPostCss;
